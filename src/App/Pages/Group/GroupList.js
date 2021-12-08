7a309442362dc6ae198a5b4a2ed082d05f8d75ec
UciBaetulloh <ucibaetulloh@gmail.com>
2021-12-08 11:22:53 +0700

    add group

---
+++ src/App/Pages/Group/GroupList.js
@@ -0,0 +1,437 @@
+import React, { Component } from "react";
+import axios from "axios";
+import ReactTable from "react-table";
+import { confirmAlert } from "react-confirm-alert";
+import "react-confirm-alert/src/react-confirm-alert.css";
+import "react-datepicker/dist/react-datepicker.css";
+import "react-table/react-table.css";
+import { serverUrl } from "../../../config.js";
+import { activeLanguage } from "../../../config";
+import { getLanguage } from "../../../languages";
+import { Box, Paper, Grid, Typography, Button } from "@mui/material";
+import { Refresh, Edit, Delete, AddBox } from "@mui/icons-material";
+const stylesListComent = {
+  inline: {
+    display: "inline",
+  },
+};
+
+class GroupListPage extends Component {
+  constructor(props) {
+    super(props);
+    this.reactTable = React.createRef();
+    this.globallang = getLanguage(activeLanguage, "global");
+    this.language = getLanguage(activeLanguage, "listadmin");
+    this.state = {
+      tableData: [],
+      filter: "",
+    };
+
+    this.tableColumns = [
+      {
+        Header: "No",
+        headerStyle: { fontWeight: "bold" },
+        accessor: "id",
+        style: { textAlign: "center" },
+        width: 100,
+      },
+      {
+        Header: "Group Name",
+        headerStyle: { fontWeight: "bold" },
+        accessor: "phoneno",
+        style: { textAlign: "center" },
+      },
+      {
+        Header: "Is Available",
+        headerStyle: { fontWeight: "bold" },
+        accessor: "name",
+        style: { textAlign: "center" },
+      },
+
+      {
+        Header: this.language.columnaction,
+        headerStyle: { fontWeight: "bold" },
+        accessor: "",
+        style: { textAlign: "center" },
+        width: 200,
+        Cell: (e) => (
+          <div>
+            <Button
+              variant="contained"
+              size="small"
+              style={{
+                backgroundColor: "#3f51b5",
+              }}
+              startIcon={<Edit />}
+              onClick={() => this.doRowEdit(e.original)}
+            >
+              <Typography
+                variant="button"
+                style={{
+                  fontSize: 14,
+                  color: "#fff",
+                  textTransform: "capitalize",
+                }}
+              >
+                {this.globallang.edit}
+              </Typography>
+            </Button>
+            &nbsp;
+            <Button
+              variant="contained"
+              size="small"
+              style={{
+                backgroundColor: "#ff0000",
+              }}
+              startIcon={<Delete />}
+              onClick={() => this.doRowDelete(e.original)}
+            >
+              <Typography
+                variant="button"
+                style={{
+                  fontSize: 14,
+                  color: "#fff",
+                  textTransform: "capitalize",
+                }}
+              >
+                {this.globallang.delete}
+              </Typography>
+            </Button>
+          </div>
+        ),
+      },
+    ];
+  }
+
+  doRowEdit = (row) => {
+    this.props.history.push("/panel/editadmin/" + row.phoneno);
+  };
+
+  doRowDelete = (row) => {
+    // console.log(row);
+    confirmAlert({
+      message: this.language.confirmdelete,
+      buttons: [
+        {
+          label: "Yes",
+          onClick: (phoneno) => {
+            var phoneno = row.phoneno;
+            // console.log(phoneno);
+            this.deleteAdmin(phoneno);
+          },
+        },
+        {
+          label: "No",
+        },
+      ],
+    });
+  };
+
+  addNew = () => {
+    this.props.history.push("/panel/inputadmin");
+  };
+
+  doSearch = () => {
+    this.props.doLoading();
+    axios
+      .post(
+        serverUrl + "admin_list.php",
+        {
+          filter: this.state.filter,
+        },
+
+        {
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+          },
+        }
+      )
+      .then((response) => {
+        this.props.doLoading();
+        var temp = this.state.tableData;
+        temp = response.data.records;
+        for (var i = 0; i < temp.length; i++) {
+          temp[i].id = i + 1;
+        }
+        this.setState({ tableData: temp });
+      })
+      .catch((error) => {
+        this.props.doLoading();
+        console.log(error);
+        alert(error);
+      });
+  };
+
+  deleteAdmin = (phoneno) => {
+    this.props.doLoading();
+    axios
+      .post(
+        serverUrl + "admin_delete.php",
+        {
+          phoneno: phoneno,
+        },
+
+        {
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+          },
+        }
+      )
+      .then((response) => {
+        this.props.doLoading();
+        alert(this.language.deletesuccess);
+        //window.location.reload()
+        this.doSearch();
+      })
+      .catch((error) => {
+        this.props.doLoading();
+        console.log(error);
+        alert(error);
+      });
+  };
+
+  componentDidMount = () => {
+    this.props.doLoading();
+    axios
+      .post(
+        serverUrl + "admin_list.php",
+        {
+          filter: "",
+        },
+
+        {
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+          },
+        }
+      )
+      .then((response) => {
+        this.props.doLoading();
+        // console.log(response.data);
+        var temp = this.state.tableData;
+        temp = response.data.records;
+        for (var i = 0; i < temp.length; i++) {
+          temp[i].id = i + 1;
+        }
+        this.setState({ tableData: temp });
+      })
+      .catch((error) => {
+        this.props.doLoading();
+        console.log(error);
+        alert(error);
+      });
+  };
+
+  reset = () => {
+    let data = "";
+    this.setState({ filter: data });
+    this.props.doLoading();
+    axios
+      .post(
+        serverUrl + "admin_list.php",
+        {
+          filter: "",
+        },
+
+        {
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+          },
+        }
+      )
+      .then((response) => {
+        this.props.doLoading();
+        // console.log(response.data);
+        var temp = this.state.tableData;
+        temp = response.data.records;
+        for (var i = 0; i < temp.length; i++) {
+          temp[i].id = i + 1;
+        }
+        this.setState({ tableData: temp });
+      })
+      .catch((error) => {
+        this.props.doLoading();
+        console.log(error);
+        alert(error);
+      });
+  };
+
+  handleCloseDialogTopUp = () => {
+    this.setState({
+      setOpenDialogPopUp: false,
+    });
+  };
+
+  renderDialogA = () => {
+    return (
+      <Dialog
+        onClose={this.handleCloseDialogTopUp}
+        aria-labelledby="customized-dialog-title"
+        open={this.state.setOpenDialogPopUp}
+        fullWidth="sm"
+        maxWidth="sm"
+      >
+        <AppBar style={stylesDialog.appBar}>
+          <Toolbar>
+            <Typography variant="h5" style={stylesDialog.title}>
+              Top Up
+            </Typography>
+            <IconButton
+              edge="end"
+              color="inherit"
+              onClick={() => this.handleCloseDialogTopUp()}
+              aria-label="close"
+            >
+              <Close />
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+        <DialogContent dividers>
+          <Box sx={{ flexGrow: 1 }}>
+            <Grid container spacing={2}>
+              <Grid item xs={2}>
+                <Typography
+                  component="span"
+                  variant="subtitle1"
+                  style={{
+                    // fontSize: 16,
+                    float: "left",
+                    marginTop: 6,
+                    color: "#005379",
+                    fontWeight: "bold",
+                    textTransform: "capitalize",
+                  }}
+                >
+                  Amount
+                </Typography>
+              </Grid>
+              <Grid item xs={10}>
+                <Input
+                  autoComplete="off"
+                  type="number"
+                  name="amount"
+                  id="amount"
+                  placeholder="Enter amount"
+                  value={this.state.amount}
+                  onChange={(event) =>
+                    this.setState({ topUpAmount: event.target.value })
+                  }
+                />
+              </Grid>
+            </Grid>
+          </Box>
+        </DialogContent>
+        <DialogActions>
+          <Button
+            variant="outlined"
+            onClick={this.doTopUpBalancePPOB}
+            color="primary"
+          >
+            Send
+          </Button>
+        </DialogActions>
+      </Dialog>
+    );
+  };
+
+  render() {
+    return (
+      <Box>
+        <Box sx={{ flexGrow: 1 }}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <Paper
+                style={{
+                  borderLeft: 6,
+                  borderColor: "#2f55a2",
+                  color: "#FFF",
+                  maxHeight: 100,
+                  padding: 16,
+                  paddingBottom: 24,
+                }}
+              >
+                <Typography
+                  component="span"
+                  variant="h1"
+                  style={
+                    (stylesListComent.inline,
+                    {
+                      fontSize: 20,
+                      color: "#006432",
+                      fontWeight: "bold",
+                    })
+                  }
+                >
+                  Group
+                </Typography>
+                <br></br>
+                <div className="contentDate">
+                  <div style={{ marginRight: 16 }}>
+                    <Button
+                      variant="contained"
+                      style={{
+                        backgroundColor: "#1273DE",
+                      }}
+                      startIcon={<Refresh />}
+                      onClick={() => this.reset()}
+                    >
+                      <Typography
+                        variant="button"
+                        style={{
+                          fontSize: 14,
+                          color: "#fff",
+                          textTransform: "capitalize",
+                        }}
+                      >
+                        {this.globallang.reset}
+                      </Typography>
+                    </Button>
+                  </div>
+                  <div style={{ marginRight: 0 }}>
+                    <Button
+                      variant="contained"
+                      style={{
+                        backgroundColor: "#008b02",
+                      }}
+                      startIcon={<AddBox />}
+                      onClick={() => this.addNew()}
+                    >
+                      <Typography
+                        variant="button"
+                        style={{
+                          fontSize: 14,
+                          color: "#fff",
+                          textTransform: "capitalize",
+                        }}
+                      >
+                        {this.globallang.add}
+                      </Typography>
+                    </Button>
+                  </div>
+                </div>
+                <br></br>
+              </Paper>
+            </Grid>
+          </Grid>
+        </Box>
+        <br></br>
+        <div className="box-container">
+          <ReactTable
+            ref={(r) => (this.reactTable = r)}
+            data={this.state.tableData}
+            columns={this.tableColumns}
+            style={{ backgroundColor: "#f2f2f2" }}
+            filterable
+            defaultFilterMethod={(filter, row) =>
+              String(row[filter.id])
+                .toLowerCase()
+                .includes(filter.value.toLowerCase())
+            }
+            defaultPageSize={5}
+          />
+        </div>
+      </Box>
+    );
+  }
+}
+export default GroupListPage;
