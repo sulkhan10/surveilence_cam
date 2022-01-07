7bf0f3eca8e24e244940ee77e8338349693ecda1
UciBaetulloh <ucibaetulloh@gmail.com>
2022-01-08 06:10:46 +0700

    playback

--- src/App/Pages/PlayBackPage/PlayBackPage.js
+++ src/App/Pages/PlayBackPage/PlayBackPage.js
@@ -1,12 +1,7 @@
 import React, { Component } from "react";
-import axios from "axios";
-import Iframe from "react-iframe";
-// import { confirmAlert } from "react-confirm-alert";
 import "react-confirm-alert/src/react-confirm-alert.css";
 import "react-datepicker/dist/react-datepicker.css";
 import "react-table/react-table.css";
-import { serverUrl } from "../../../config.js";
-import SelectMultiColumn from "../../Components/SelectMultiColumn/SelectMultiColumn";
 import { Box, Paper, Grid, Typography } from "@mui/material";
 
 const stylesListComent = {
@@ -26,79 +21,9 @@ class PlayBackPage extends Component {
     };
   }
 
-  changeGroup = (groupId) => {
-    this.setState({
-      groupId: groupId,
-    });
-    this.setState({ cameraShow: [], cameraId: "" });
-    this.cameraList(groupId);
-  };
-
-  changeCamera = (cameraId) => {
-    this.setState({
-      cameraId: cameraId,
-    });
-  };
-
-  componentDidMount = () => {
-    this.groupList();
-  };
-
-  groupList = () => {
-    axios
-      .post(
-        serverUrl + "group_list.php",
-        {},
-
-        {
-          headers: {
-            "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
-          },
-        }
-      )
-      .then((response) => {
-        var temp = this.state.groupShow;
-        temp = response.data.records;
-        this.setState({ groupShow: temp, groupId: temp[0].groupId });
-        this.cameraList(temp[0].groupId);
-      })
-      .catch((error) => {
-        console.log(error);
-        alert(error);
-      });
-  };
-
-  cameraList = (groupId) => {
-    axios
-      .post(
-        serverUrl + "device_list.php",
-        { groupId: groupId },
-
-        {
-          headers: {
-            "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
-          },
-        }
-      )
-      .then((response) => {
-        // console.log(response);
-        if (response.data.records.length > 0) {
-          var temp = this.state.cameraShow;
-          temp = response.data.records;
-          this.setState({ cameraShow: temp, cameraId: temp[0].deviceId });
-        } else {
-          this.setState({ cameraShow: [], cameraId: "" });
-        }
-      })
-      .catch((error) => {
-        console.log(error);
-        alert(error);
-      });
-  };
+  componentDidMount = () => {};
 
   render() {
-    const urlCamera = this.state.url + "?groupby=" + this.state.groupId;
-
     return (
       <Box>
         <Box sx={{ flexGrow: 1 }}>
@@ -128,69 +53,54 @@ class PlayBackPage extends Component {
                 >
                   Playback
                 </Typography>
-                <br></br>
-                <div className="contentDate">
-                  <Typography
-                    component="span"
-                    variant="subtitle1"
-                    style={
-                      (stylesListComent.inline,
-                      {
-                        marginRight: 16,
-                        justifyContent: "center",
-                        alignContent: "center",
-                        alignItems: "center",
-                        alignSelf: "center",
-                        color: "#006432",
-                        fontWeight: "bold",
-                      })
-                    }
-                  >
-                    Group By:
-                  </Typography>
-                  <div style={{ marginRight: 16 }}>
-                    <SelectMultiColumn
-                      width={200}
-                      value={this.state.groupId}
-                      valueColumn={"groupId"}
-                      showColumn={"groupName"}
-                      columns={["groupName"]}
-                      data={this.state.groupShow}
-                      onChange={this.changeGroup}
-                    />
-                  </div>
-                  <Typography
-                    component="span"
-                    variant="subtitle1"
-                    style={
-                      (stylesListComent.inline,
-                      {
-                        marginRight: 16,
-                        justifyContent: "center",
-                        alignContent: "center",
-                        alignItems: "center",
-                        alignSelf: "center",
-                        color: "#006432",
-                        fontWeight: "bold",
-                      })
-                    }
-                  >
-                    Camera:
-                  </Typography>
-                  <div style={{ marginRight: 0 }}>
-                    <SelectMultiColumn
-                      width={200}
-                      value={this.state.cameraId}
-                      valueColumn={"deviceId"}
-                      showColumn={"deviceName"}
-                      columns={["deviceName"]}
-                      data={this.state.cameraShow}
-                      onChange={this.changeCamera}
-                    />
-                  </div>
-                </div>
-                <br></br>
               </Paper>
+            </Grid>
+          </Grid>
+        </Box>
+        <br></br>
+        <Box sx={{ flexGrow: 1 }}>
+          <Grid container spacing={2}>
+            <Grid item xs={4}>
+              <Grid item xs={12}>
+                <Paper
+                  style={{
+                    padding: "10px",
+                    backgroundColor: "#fff",
+                    height: "75vh",
+                  }}
+                >
+                  <div className="page-header">
+                    <Typography
+                      component="span"
+                      variant="h2"
+                      style={{
+                        fontSize: 16,
+                        color: "#006432",
+                        fontWeight: "bold",
+                        textTransform: "capitalize",
+                        float: "left",
+                      }}
+                    >
+                      Camera Channel
+                    </Typography>
+
+                    <span className="dash">&nbsp;&nbsp;</span>
+                  </div>
+                </Paper>
+              </Grid>
+            </Grid>
+            <Grid item xs={8}>
+              <Grid item xs={12}>
+                <Paper
+                  style={{
+                    padding: "10px",
+                    backgroundColor: "#fff",
+                    height: "75vh",
+                  }}
+                >
+                  asdad
+                </Paper>
+              </Grid>
             </Grid>
           </Grid>
         </Box>
